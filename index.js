3a1b954500f65073564e5040f9648955738e6f9c
Samuel Macleod <samuel@macleod.space>
2020-09-13 13:36:26 +0100

    Remove auth

--- index.js
+++ index.js
@@ -10,13 +10,7 @@ const app = express();
 app.use(cors());
 const port = process.env.PORT || 8764;
 const spacesEndpoint = new AWS.Endpoint("nyc3.digitaloceanspaces.com");
-const GhostAdminAPI = require("@tryghost/admin-api");
-const ghostToken = process.env.GHOST_TOKEN;
-const Admin = new GhostAdminAPI({
-  url: "https://content.freshair.org.uk",
-  key: ghostToken,
-  version: "v3"
-});
+
 const Form = mongoose.model(
   `Form`,
   new mongoose.Schema(
@@ -83,151 +77,13 @@ app.get("/spec/:slug", async (req, res) => {
   }
 });
 
-app.get("/results/contract", async (req, res) => {
-  try {
-    let ret = await Form.find({ slug: "contract" }, ["slug", "user"]);
-    return res.json(ret);
-  } catch (e) {
-    console.error(e);
-    return res.sendStatus(500);
-  }
-});
-
-app.get("/results/stand", async (req, res) => {
-  candidates = await Form.find({ slug: "stand" }, [
-    "slug",
-    "createdAt",
-    "user",
-    "data",
-    "path"
-  ]);
-  candidates = await Promise.all(
-    candidates.map(async c => {
-      let u = await Admin.users.read({ slug: c.user });
-      return {
-        ...c,
-        user: {
-          slug: u.slug,
-          email: u.email,
-          pic: u.profile_image
-            ? u.profile_image.startsWith("http")
-              ? u.profile_image
-              : `https://cdn.freshair.dev/upload/${u.profile_image}`
-            : "",
-          name: u.name
-        }
-      };
-    })
-  );
-  return res.json(ret);
-});
-app.get("/results/apply", async (req, res) => {
-  try {
-    let ret = await Promise.all(
-      (
-        await Form.find({ slug: "apply" }, [
-          "slug",
-          "createdAt",
-          "user",
-          "data",
-          "path"
-        ])
-      )
-        .map(f => {
-          let data = {};
-          [...f.path].forEach(k => (data[k] = f.data[k]));
-          return {
-            user: f.user,
-            slug: f.slug,
-            createdAt: f.createdAt,
-            path: f.path,
-            data
-          };
-        })
-        .map(async f => {
-          if (Array.isArray(f.data.show_slug)) {
-            f.data.show_slug = f.data.show_slug[0];
-          }
-          if (f.data.show_slug) {
-            try {
-              let show = await Admin.posts.read({
-                slug: f.data.show_slug
-              });
-              console.log(show);
-              f.data.show = {
-                slug: show.slug,
-                name: show.title,
-                hosts: show.authors.map(a => ({
-                  email: a.email,
-                  slug: a.slug,
-                  pic: a.profile_image,
-                  name: a.name
-                })),
-                description: show.excerpt,
-                demo: "",
-                pic: show.feature_image
-              };
-            } catch (e) {
-              console.error(e);
-            }
-          } else if (f.data.show_details) {
-            f.data.show = {
-              name: f.data.show_details.name,
-              hosts: await Promise.all(
-                [...new Set([f.user, ...f.data.show_people])]
-                  .filter(u => u)
-                  .map(async s => {
-                    try {
-                      let u = await Admin.users.read({ slug: s });
-                      return {
-                        slug: u.slug,
-                        email: u.email,
-                        pic: u.profile_image
-                          ? u.profile_image.startsWith("http")
-                            ? u.profile_image
-                            : `https://cdn.freshair.dev/upload/${u.profile_image}`
-                          : "",
-                        name: u.name
-                      };
-                    } catch (e) {
-                      console.log(e);
-                      return s;
-                    }
-                  })
-              ),
-              description: f.data.show_details.description,
-              demo: `https://cdn.freshair.dev/upload/${f.data.show_demo}`,
-              pic: `https://cdn.freshair.dev/upload/${f.data.show_pic}`
-            };
-          }
-          return f;
-        })
-    );
-    return res.json(ret);
-  } catch (e) {
-    return res.status(500).send(e);
-  }
-});
 app.post("/submit/:slug", async (req, res) => {
   try {
     let { data, path } = req.body;
-    let auth = req.headers["x-auth-token"];
     let slug = req.params.slug.replace(/[^a-zA-Z-]+/g, "-").toLowerCase();
-    let verify = await fetch(
-      process.env.AUTH_API || `http://localhost:8007/verify`,
-      {
-        method: "POST",
-        headers: {
-          "X-Auth-Token": auth
-        }
-      }
-    ).then(r => r.json());
-    if (!verify.ok) {
-      console.log(verify);
-      return res.status(401).send(verify);
-    }
+
     await Form.create({
-      user: verify.slug,
+      user: "freshair-member",
       slug,
       data,
       path
